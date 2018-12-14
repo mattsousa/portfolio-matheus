528022548c8bd52ee50c6e65feb13f7019141263
Matheus Sousa <matheus.sousa1ms@gmail.com>
2018-12-14 10:21:24 -0400

    Index pronto

---
+++ academic.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+  <head>
+    <title>Mattsousa</title>
+    <meta lang="pt-br">
+    <meta name="author" name="Matheus Sousa">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/css/bootstrap.min.css">
+    <link rel="stylesheet" href="/css/style.css">
+  </head>
+  <body>
+    <header>
+      <nav class="navbar navbar-expand-lg navbar-dark fixed-top bg-dark">
+        <a class="navbar-brand" href="index.php">
+          <span>
+            <img src="/image/matticon.png" alt="" class="icon">
+            Portfólio do Matheus
+          </span>
+        </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+              <a class="nav-link" href="index.php">Home</a>
+            </li>
+            <li class="nav-item active">
+              <a class="nav-link" href="academic.php">Acadêmico</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="works.php">Trabalhos</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="contact.php">Contato</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </header>
+    <main>
+
+    </main>
+    <footer class="footer text-center text-light">
+      <div class="row infos">
+        <div class="col-md-12 col-lg-4 mb-2">
+          <h2 class="text-uppercase">Localização</h2>
+          <p>
+            Bairro Adrianópolis, Manaus, Amazonas
+          </p>
+        </div>
+        <div class="col-md-12 col-lg-4 mb-2">
+          <h2 class="text-uppercase">Redes Sociais</h2>
+          <div class="row">
+            <div class="col-3">
+              <img class="img-fluid" src="https://via.placeholder.com/64x64?text=place" alt="">
+            </div>
+            <div class="col-3">
+              <img class="img-fluid" src="https://via.placeholder.com/64x64?text=place" alt="">
+            </div>
+            <div class="col-3">
+              <img class="img-fluid" src="https://via.placeholder.com/64x64?text=place" alt="">
+            </div>
+            <div class="col-3">
+              <img class="img-fluid" src="https://via.placeholder.com/64x64?text=place" alt="">
+            </div>
+          </div>
+        </div>
+        <div class="col-md-12 col-lg-4 mb-2">
+          <h2 class="text-uppercase">Sobre o site</h2>
+          <p class="text-left">
+            Este site é uma criação minha, Matheus Pinheiro de Sousa, Desenvolvedor de Websites, Desenvolvedor de apps para Android e Empreendedor.
+          </p>
+        </div>
+      </div>
+      <div class="copyright text-center">
+        <p>Copyright &copy Matheus Sousa 2018 </p>
+      </div>
+    </footer>
+    <script type="text/javascript" src="/js/jquery.js"></script>
+    <script type="text/javascript" src="/js/popper.js"></script>
+    <script type="text/javascript" src="/js/bootstrap.min.js"></script>
+  </body>
+</html>
---
+++ index.php
@@ -0,0 +1,113 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+  <head>
+    <title>Mattsousa</title>
+    <meta lang="pt-br">
+    <meta name="author" name="Matheus Sousa">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/css/bootstrap.min.css">
+    <link rel="stylesheet" href="/css/style.css">
+  </head>
+  <body>
+    <header>
+      <nav class="navbar navbar-expand-lg navbar-dark fixed-top bg-dark">
+        <a class="navbar-brand" href="index.php">
+          <span>
+            <img src="/image/matticon.png" alt="" class="icon">
+            Portfólio do Matheus
+          </span>
+        </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item active">
+              <a class="nav-link" href="index.php">Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="academic.php">Acadêmico</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="works.php">Trabalhos</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="contact.php">Contato</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </header>
+    <main>
+      <div class="panel bg-primary text-center">
+        <img src="/image/profile.png" alt="" class="img-thumbnail">
+        <h2 class="text-light text-center">Hey, Eu sou o Matheus</h2>
+        <p class="lead text-center container">
+          <a class="btn btn-outline-dark btn-block" href="#" role="button">Clique aqui para ver minhas habilidades</a>
+          <a class="btn btn-outline-light btn-block" href="#" role="button">Ou aqui para baixar o meu currículo</a>
+        </p>
+      </div>
+      <hr>
+      <div class="container jumbotron">
+        <h2>Sobre mim</h2>
+        <p>
+          Me chamo Matheus Pinheiro de Sousa, natural de Belém do Pará, Brasil. Sou um desenvolvedor de softwares com um vasto conhecimento de computação, altamente criativo, proativo e sempre pronto para novos desafios. Criei este site para divulgar minhas habilidades e funcionará como um projeto pessoal para estudo e diversão também :D.
+        </p>
+        <p>
+          Atualmente, sou estudante universitário matriculado no curso de Sistemas de Informação da Universidade do Estado do Amazonas(UEA). Já estudei em outras instituições em busca de mais conhecimento, mas sempre em cursos voltados ao desenvolvimento de softwares.
+        </p>
+        <p>
+          Com 5 anos de computação, já tive experiências com várias plataformas de desenvolvimento e apesar de ter preferência com programação em Java, posso trabalhar com <em>qualquer</em> linguagem, tecnologia sem problema algum. Para saber dos detalhes, <a href="#">clique aqui</a> e veja meus trabalhos mais recentes com seus próprios olhos.
+        </p>
+        <blockquote class="blockquote">
+          <p class="mb-0">
+            Sempre há um início para qualquer grande jornada. Mas sua continuação até que esteja completamente finalizada guarda a verdadeira glória
+          </p>
+          <footer class="blockquote-footer">
+            Sir Francis Drake
+          </footer>
+        </blockquote>
+      </div>
+    </main>
+    <footer class="footer text-center text-light">
+      <div class="row infos">
+        <div class="col-md-12 col-lg-4 mb-2">
+          <h2 class="text-uppercase">Localização</h2>
+          <p>
+            Bairro Adrianópolis, Manaus, Amazonas
+          </p>
+        </div>
+        <div class="col-md-12 col-lg-4 mb-2">
+          <h2 class="text-uppercase">Redes Sociais</h2>
+          <div class="row">
+            <div class="col-3">
+              <img class="img-fluid" src="https://via.placeholder.com/64x64?text=place" alt="">
+            </div>
+            <div class="col-3">
+              <img class="img-fluid" src="https://via.placeholder.com/64x64?text=place" alt="">
+            </div>
+            <div class="col-3">
+              <img class="img-fluid" src="https://via.placeholder.com/64x64?text=place" alt="">
+            </div>
+            <div class="col-3">
+              <img class="img-fluid" src="https://via.placeholder.com/64x64?text=place" alt="">
+            </div>
+          </div>
+        </div>
+        <div class="col-md-12 col-lg-4 mb-2">
+          <h2 class="text-uppercase">Sobre o site</h2>
+          <p class="text-left">
+            Este site é uma criação minha, Matheus Pinheiro de Sousa, Desenvolvedor de Websites, Desenvolvedor de apps para Android e Empreendedor.
+          </p>
+        </div>
+      </div>
+      <div class="copyright text-center">
+        <p>Copyright &copy Matheus Sousa 2018 </p>
+      </div>
+    </footer>
+    <script type="text/javascript" src="/js/jquery.js"></script>
+    <script type="text/javascript" src="/js/popper.js"></script>
+    <script type="text/javascript" src="/js/bootstrap.min.js"></script>
+  </body>
+</html>
